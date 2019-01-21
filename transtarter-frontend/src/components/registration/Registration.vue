22df6316159682db25c3865de9263a08c9f858da
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-21 17:33:37 +0200

    finish mobile registration

--- transtarter-frontend/src/components/registration/Registration.vue
+++ transtarter-frontend/src/components/registration/Registration.vue
@@ -7,30 +7,109 @@
     <!-- mobile version -->
     <div class="mobile-version-block">
 
-      <carousel class="mobile-version-block-header">
-        <slide>
+      <carousel
+        perPage='1'
+        class="mobile-version-block-carousel"
+      >
+        <slide class="mobile-version-block-header">
           <div class="reg-in-our-shop bold text-center">
             Зарегистрируйтесь <br>
             в нашем магазине, чтобы
           </div>
           <div class="rows-and-gear-icon"></div>
-          <div class="order-management">
+          <div class="order-management text-center">
             Управлять своими заказами в личном кабинете: следить за статусом оплаты
             и доставки, хранить историю заказов
           </div>
         </slide>
-        <slide>
+        <slide class="mobile-version-block-header">
           <div class="reg-in-our-shop bold text-center">
             Зарегистрируйтесь <br>
             в нашем магазине, чтобы
           </div>
           <div class="rows-and-gear-icon"></div>
-          <div class="order-management">
+          <div class="order-management text-center">
             Управлять своими заказами в личном кабинете: следить за статусом оплаты
             и доставки, хранить историю заказов
           </div>
         </slide>
+
       </carousel>
+
+      <div class="mobile-version-block-main">
+        <div class="registration-text text-center">
+          Регистрация
+        </div>
+        <form class="registration-form">
+          <div class="form-group">
+            <label class="label">Введите имя и фамилию контактного лица</label>
+            <input
+              v-model="fullName"
+              placeholder="Имя и фамилия"
+              class="form-control"
+            >
+          </div>
+          <div class="form-group">
+            <label class="label">Введите телефон</label>
+            <input
+              v-model="phone"
+              placeholder="Телефон"
+              class="form-control"
+            >
+          </div>
+          <div class="form-group">
+            <label class="label">Введите email</label>
+            <input
+              v-model="email"
+              placeholder="Почта"
+              class="form-control"
+            >
+          </div>
+          <div class="form-group">
+            <label class="label">Какую организацию вы представляете?</label>
+            <select class="form-control">
+              <option>Автосервис</option>
+              <option>Автосервис</option>
+              <option>Автосервис</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label class="label">Введите наименование организации</label>
+            <div class="two-selectors">
+
+              <select class="form-control first-selector">
+                <option>ООО</option>
+                <option>ООО</option>
+                <option>ООО</option>
+              </select>
+              <input
+                v-model="orgName"
+                placeholder="Название огранизации"
+                class="form-control second-selector"
+              >
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="btn btn-orange btn-block"
+          >
+            Зарегистрироваться
+          </button>
+
+          <div class="accept-checkbox text-center">
+            Нажимая на кнопку, вы даете согласие
+            на обработку своих персональных данных
+            и соглашаетесь с <a href="#" class="policy-link">Политикой</a> конфиденциальности
+          </div>
+
+        </form>
+
+        <div class="already-have-account text-center">
+          <p>Уже зарегистрированы</p>
+          <a href="#" class="go-log-in">Войти</a>
+        </div>
+      </div>
 
     </div>
   </div>
