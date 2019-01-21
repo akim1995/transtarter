04706a7d287c8231f6a8ab5a7ddd50934e42d2df
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-21 14:52:22 +0200

    init vuejs project

---
+++ transtarter-frontend/src/components/Registration.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- desktop version -->
+
+    <!-- mobile version -->
+
+  </div>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
