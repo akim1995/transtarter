d1a5bde886b4c8d7971b67f9bbad4b385a5aa734
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-22 17:28:02 +0200

    add success modal for restore password

--- transtarter-frontend/src/components/password-restore/password-restore.vue
+++ transtarter-frontend/src/components/password-restore/password-restore.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="password-restore wrapper">
     <!-- desktop and mobile version -->
-    <div class="log-in-modal modal-popup">
+    <div
+      class="log-in-modal modal-popup"
+      v-if="restore.beforeRestore"
+    >
       <div class="close">✖</div>
       <div class="title bold">
         Восстановление пароля
@@ -20,12 +23,31 @@
         <button
           type="submit"
           class="btn btn-orange reset-btn"
+          @click='restorePassword()'
         >
           Восстановить пароль
         </button>
       </div>
     </div>
     <!-- end of desktop and mobile version -->
+
+    <!-- success restore -->
+    <div
+      class="log-in-modal modal-popup"
+      v-if="restore.successRestore"
+    >
+      <div class="close">✖</div>
+      <div class="title bold">
+        Мы отправили ссылку для восстановления
+        на email
+      </div>
+
+      <div class="tip" style="margin-bottom: 0;">
+        Перейдите по ней и следуйте инструкциям на сайте.
+      </div>
+
+    </div>
+    <!-- end success restore -->
   </div>
 </template>
 
@@ -35,6 +57,16 @@ import { Component, Vue } from 'vue-property-decorator'
 @Component
 export default class PasswordRestore extends Vue {
   email = '';
+  restore = {
+    beforeRestore: true,
+    successRestore: null
+  };
+
+  restorePassword () {
+    // here will be http response
+    this.restore.beforeRestore = false
+    this.restore.successRestore = true
+  }
 }
 </script>
 
--- transtarter-frontend/src/views/Home.vue
+++ transtarter-frontend/src/views/Home.vue
@@ -2,15 +2,15 @@
   <div class="home">
     <!-- <Registration /> -->
     <!-- <LogIn /> -->
-    <PasswordRestore/>
+    <PasswordRestore />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Registration from '@/components/registration/registration.vue' // @ is an alias to /src
-import LogIn from '@/components/log-in/log-in.vue' // @ is an alias to /src
-import PasswordRestore from '@/components/password-restore/password-restore.vue' // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+import Registration from '@/components/registration/registration.vue'; // @ is an alias to /src
+import LogIn from '@/components/log-in/log-in.vue'; // @ is an alias to /src
+import PasswordRestore from '@/components/password-restore/password-restore.vue'; // @ is an alias to /src
 
 @Component({
   components: {
@@ -20,11 +20,9 @@ import PasswordRestore from '@/components/password-restore/password-restore.vue'
   }
 })
 export default class Home extends Vue {}
-
 </script>
 
 <style scoped lang="scss">
-
 .home {
   width: 100vw;
   height: 100%;
