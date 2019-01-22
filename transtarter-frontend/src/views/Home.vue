bd1d906260c2a24000d9159e89e704c587073731
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-22 17:38:01 +0200

    small refactoring

--- transtarter-frontend/src/views/Home.vue
+++ transtarter-frontend/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home">
     <!-- <Registration /> -->
-    <!-- <LogIn /> -->
-    <PasswordRestore />
+    <LogIn />
+    <!-- <PasswordRestore /> -->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Registration from '@/components/registration/registration.vue'; // @ is an alias to /src
-import LogIn from '@/components/log-in/log-in.vue'; // @ is an alias to /src
-import PasswordRestore from '@/components/password-restore/password-restore.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator'
+import Registration from '@/components/registration/registration.vue' // @ is an alias to /src
+import LogIn from '@/components/log-in/log-in.vue' // @ is an alias to /src
+import PasswordRestore from '@/components/password-restore/password-restore.vue' // @ is an alias to /src
 
 @Component({
   components: {
