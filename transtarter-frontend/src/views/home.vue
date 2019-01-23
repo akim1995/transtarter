f7233f13513f23b2a4797a292371df727909f680
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-23 19:30:18 +0200

    return back home view

---
+++ transtarter-frontend/src/views/home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <Registration />
+    <!-- <LogIn /> -->
+    <!-- <PasswordRestore /> -->
+
+    <!-- <div class="login">
+      <button @click="login">Login</button>
+      <button @click="logout">Logout</button>
+    </div> -->
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Registration from '@/components/authorization/registration/registration.vue'
+import LogIn from '@/components/authorization/log-in/log-in.vue'
+import PasswordRestore from '@/components/authorization/password-restore/password-restore.vue'
+import AuthService from '../services/auth.service' // idk why @ doesn't work here
+
+const auth = new AuthService()
+
+@Component({
+  components: {
+    Registration,
+    LogIn,
+    PasswordRestore
+  }
+})
+export default class Home extends Vue {
+  public currentUser: string = '';
+  public accessTokenExpired: boolean | undefined = false;
+  public isLoggedIn: boolean = false;
+
+  get username (): string {
+    return this.currentUser
+  }
+
+  public login () {
+    auth.login()
+  }
+
+  public logout () {
+    auth.logout()
+  }
+
+  public mounted () {
+    debugger
+    auth.getUser().then(user => {
+      this.currentUser = user.profile.name
+      this.accessTokenExpired = user.expired
+
+      this.isLoggedIn = user !== null && !user.expired
+    })
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  width: 100vw;
+  height: 100%;
+}
+</style>
