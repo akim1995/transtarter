0a08dad76242de49d9a15edeb544fc35e60f490e
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-23 15:06:38 +0200

    work with oidc

--- transtarter-frontend/src/views/Home.vue
+++ transtarter-frontend/src/views/Home.vue
@@ -2,15 +2,20 @@
   <div class="home">
     <!-- <Registration /> -->
     <!-- <LogIn /> -->
-    <PasswordRestore />
+    <!-- <PasswordRestore /> -->
+
+    <button @click="login">Login</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import Registration from '@/components/authorization/registration/registration.vue' // @ is an alias to /src
-import LogIn from '@/components/authorization/log-in/log-in.vue' // @ is an alias to /src
-import PasswordRestore from '@/components/authorization/password-restore/password-restore.vue' // @ is an alias to /src
+import Registration from '@/components/authorization/registration/registration.vue'
+import LogIn from '@/components/authorization/log-in/log-in.vue'
+import PasswordRestore from '@/components/authorization/password-restore/password-restore.vue'
+import AuthService from '../services/auth.service' // idk why @ doesn't work here
+
+const auth = new AuthService()
 
 @Component({
   components: {
@@ -19,7 +24,33 @@ import PasswordRestore from '@/components/authorization/password-restore/passwor
     PasswordRestore
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public currentUser: string = '';
+  public accessTokenExpired: boolean | undefined = false;
+  public isLoggedIn: boolean = false;
+
+  get username (): string {
+    return this.currentUser
+  }
+
+  public login () {
+    auth.login()
+  }
+
+  public logout () {
+    auth.logout()
+  }
+
+  public mounted () {
+    debugger
+    auth.getUser().then(user => {
+      this.currentUser = user.profile.name
+      this.accessTokenExpired = user.expired
+
+      this.isLoggedIn = user !== null && !user.expired
+    })
+  }
+}
 </script>
 
 <style scoped lang="scss">
