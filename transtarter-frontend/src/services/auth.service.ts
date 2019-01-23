d1c5d01ab39bf52539c0f4c1f8e710a1782ae04d
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-23 15:53:10 +0200

    some settings changes

--- transtarter-frontend/src/services/auth.service.ts
+++ transtarter-frontend/src/services/auth.service.ts
@@ -1,41 +1,42 @@
 import { UserManager, WebStorageStateStore, User } from 'oidc-client'
 
 export default class AuthService {
-    private userManager: UserManager;
+  private userManager: UserManager;
 
-    constructor () {
-      const AUTH0_DOMAIN: string = 'http://144.76.133.147:5000'
+  constructor () {
+    const AUTH0_DOMAIN: string = 'http://144.76.133.147:5000'
+    // const AUTH0_DOMAIN: string = 'http://192.168.2.12:5000'
 
-      const settings: any = {
-        userStore: new WebStorageStateStore({ store: window.localStorage }),
-        authority: AUTH0_DOMAIN,
-        client_id: 'skoruba_identity_admin',
-        redirect_uri: 'http://localhost:8080/callback.html',
-        response_type: 'id_token token',
-        scope: 'openid profile',
-        post_logout_redirect_uri: 'http://localhost:8080/',
-        filterProtocolClaims: true,
-        metadata: {
-          issuer: 'identity',
-          authorization_endpoint: AUTH0_DOMAIN + '/connect/authorize',
-          userinfo_endpoint: AUTH0_DOMAIN + '/connect/userinfo',
-          end_session_endpoint: AUTH0_DOMAIN + '/connect/endsession',
-          jwks_uri: AUTH0_DOMAIN + '/.well-known/openid-configuration/jwks'
-        }
+    const settings: any = {
+      userStore: new WebStorageStateStore({ store: window.localStorage }),
+      authority: AUTH0_DOMAIN,
+      client_id: 'skoruba_identity_admin',
+      redirect_uri: 'http://localhost:8080/callback.html',
+      response_type: 'id_token token',
+      scope: 'openid profile',
+      post_logout_redirect_uri: 'http://localhost:8080/',
+      filterProtocolClaims: true,
+      metadata: {
+        issuer: 'identity',
+        authorization_endpoint: AUTH0_DOMAIN + '/connect/authorize',
+        userinfo_endpoint: AUTH0_DOMAIN + '/connect/userinfo',
+        end_session_endpoint: AUTH0_DOMAIN + '/connect/endsession',
+        jwks_uri: AUTH0_DOMAIN + '/.well-known/openid-configuration/jwks'
       }
-
-      this.userManager = new UserManager(settings)
     }
 
-    public getUser (): Promise<User> {
-      return this.userManager.getUser()
-    }
+    this.userManager = new UserManager(settings)
+  }
 
-    public login (): Promise<void> {
-      return this.userManager.signinRedirect()
-    }
+  public getUser (): Promise<User> {
+    return this.userManager.getUser()
+  }
 
-    public logout (): Promise<void> {
-      return this.userManager.signinRedirect()
-    }
+  public login (): Promise<void> {
+    return this.userManager.signinRedirect()
+  }
+
+  public logout (): Promise<void> {
+    return this.userManager.signinRedirect()
+  }
 }
