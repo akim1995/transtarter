870dc61936999ff7b8494296a66e41f790fc7ca1
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-24 17:49:10 +0200

    add inline form-group

--- transtarter-frontend/src/components/user-profile/profile-settings/profile-settings.vue
+++ transtarter-frontend/src/components/user-profile/profile-settings/profile-settings.vue
@@ -9,17 +9,36 @@
       role="group"
       aria-label="Basic example"
     >
-      <button type="button" class="active">Регистрационные данные</button>
+      <button
+        type="button"
+        class="active"
+      >Регистрационные данные</button>
       <button type="button">Адреса доставки</button>
       <button type="button">Реквизиты и юр. лица</button>
     </div>
 
     <div class="change-avatar">
-        <div class="user-picture"></div>
-        <div class="change-user-picture">
-            Изменить
-        </div>
+      <div class="user-picture"></div>
+      <div class="change-user-picture">
+        Изменить
+      </div>
     </div>
+
+    <form>
+      <div class="form-group row">
+        <label
+          class="col-sm-4 big-label"
+        >Название компании</label>
+        <div class="col-sm-6">
+          <input
+            type="text"
+            class="form-control-lg"
+            placeholder="email@example.com"
+          >
+        </div>
+
+  </div>
+  </form>
 
   </div>
 </template>
