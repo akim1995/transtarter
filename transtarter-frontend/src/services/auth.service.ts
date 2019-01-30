0f83acc05c0dddff24c0b859a41dd3c4e6d2662c
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-30 16:13:44 +0200

    fix settings

--- transtarter-frontend/src/services/auth.service.ts
+++ transtarter-frontend/src/services/auth.service.ts
@@ -5,20 +5,19 @@ export default class AuthService {
 
   constructor () {
     const AUTH0_DOMAIN: string = 'http://144.76.133.147:5000'
-    const MY_HOST: any = process.env.VUE_APP_HOST
+    const MY_HOST: string = process.env.VUE_APP_HOST
 
     const settings: UserManagerSettings = {
       userStore: new WebStorageStateStore({ store: window.localStorage }),
       authority: AUTH0_DOMAIN,
       client_id: 'skoruba_identity_admin',
-      redirect_uri: `${MY_HOST}/callback.html/`,
+      redirect_uri: `${MY_HOST}/callback.html`,
       post_logout_redirect_uri: `${MY_HOST}/`,
       response_type: 'id_token token',
       scope: 'openid profile roles',
       filterProtocolClaims: true,
       loadUserInfo: true,
       metadata: {
-
         issuer: 'identity',
         authorization_endpoint: AUTH0_DOMAIN + '/connect/authorize',
         userinfo_endpoint: AUTH0_DOMAIN + '/connect/userinfo',
