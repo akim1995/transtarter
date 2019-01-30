6c85df32adca9da663ef2e31b0329f71d0cc318b
SerovMikhail <serov.mikhail.job@gmail.com>
2019-01-30 10:51:55 +0200

    use router again

--- transtarter-frontend/src/main.ts
+++ transtarter-frontend/src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import router from './router' // we don't use router at all right now
+import router from './router' // we don't use router at all right now
 import { store } from './store/index'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -18,7 +18,7 @@ Vue.config.productionTip = false
 export const eventBus = new Vue()
 
 new Vue({
-  // router,
+  router,
   store,
   render: h => h(App)
 }).$mount('#app')
--- transtarter-frontend/src/router.ts
+++ transtarter-frontend/src/router.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/home.vue'
 import UserProfile from './views/user-profile.vue'
 
 Vue.use(Router)
