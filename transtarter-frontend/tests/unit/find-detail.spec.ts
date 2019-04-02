70bde701df714519dff93281fa131c83fef15d6d
akim1995 <akim.khalitov.ya@gmail.com>
2019-04-02 17:21:52 +0200

    Unit tests for find-detail component

--- transtarter-frontend/tests/unit/find-detail.spec.ts
+++ transtarter-frontend/tests/unit/find-detail.spec.ts
@@ -2,15 +2,26 @@ import { mount } from '@vue/test-utils'
 import findDetail from '@/components/find-detail/find-detail.vue'
 describe('Renders correct html', () => {
   const wrapper = mount(findDetail)
+  const buttons = wrapper.findAll('.find-detail__filter-item')
+
   it('Has an input tag', () => {
     expect(wrapper.contains('input')).toBe(true)
   })
 
-  // it('test2', () => {
-  //   const wrapper = mount(findDetail)
-  //   const button = wrapper.find('.find-detail__filter-item')
-  //   button.trigger('click')
-  //   console.log(wrapper.vm)
-  //   // expect(wrapper.vm.activeTab.toBe(0)
-  // })
+  it('changes activeTab property on click', () => {
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons.at(i)
+      button.trigger('click')
+      expect(wrapper.vm.$data.activeTab).toBe(i)
+    }
+  })
+
+  it('changes input placeholder value on tab click', () => {
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons.at(i)
+      button.trigger('click')
+      const input = wrapper.find('.find-detail__input')
+      expect(input.attributes('placeholder')).toBe(wrapper.vm.$data.placeholderText[i])
+    }
+  })
 })
