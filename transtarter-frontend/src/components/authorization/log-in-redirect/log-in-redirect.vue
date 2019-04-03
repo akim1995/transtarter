abf5697d83c5f61dae179815f2bcfe5f105b6898
SerovMikhail <serov.mikhail.job@gmail.com>
2019-04-03 16:11:12 +0200

    add login component

---
+++ transtarter-frontend/src/components/authorization/log-in-redirect/log-in-redirect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      Происходит редирект на Transtarter Identity Server
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { store } from '@/store/index'
+
+@Component
+export default class LogIn extends Vue {
+  mounted () {
+    store.dispatch('auth/login')
+  }
+}
+</script>
