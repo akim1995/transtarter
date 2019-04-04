a1d36c1928ec4fd391b24acab874f42972b45c36
akim1995 <akim.khalitov.ya@gmail.com>
2019-04-04 15:37:49 +0200

    buttons for styleguide

--- transtarter-frontend/src/components/shared/styleguide.vue
+++ transtarter-frontend/src/components/shared/styleguide.vue
@@ -2,7 +2,38 @@
   <div>
     <div class="container">
 
+    <p>Buttons</p>
+    <br>
+    <button class="btn btn-outline">
+      btn small
+    </button>
+    <br>
+    <br>
+    <button class="btn btn-md btn-outline">
+      btn medium
+    </button>
+    <br><br>
+
+    <button disabled class="btn btn-md btn-outline">
+      btn medium
+    </button>
+    <br>
+    <br>
+    <button class="btn btn-yellow">
+      btn medium
+    </button>
+
+    <br><br>
+    <button class="btn btn-md btn-yellow">
+      btn medium yellow
+    </button>
+    <br><br>
+    <button class="btn btn-lg btn-yellow">
+      btn large yellow
+    </button>
     <!-- RADIO BUTTON -->
+    <br>
+    <br>
     <H2>radio button</H2>
     <br>
     <input type="radio" id="radio1" name="deliver-address" class="radio">
